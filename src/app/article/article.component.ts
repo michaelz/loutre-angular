6d8c0340be633bf61f82aa87cb3eb39d1266f27e
Michael Zwyssig <mzwyssig@3ne.ch>
2018-05-04 11:21:48 +0200

    decodeURI for google referenced paths (slugs) - redirect

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -5,12 +5,13 @@ import { AppComponent } from '../app.component';
 import { Title } from '@angular/platform-browser';
 import { routerTransition } from '../router.animations';
 import { Meta } from '@angular/platform-browser';
+import { Location } from '@angular/common';
 
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  animations: [ routerTransition ],
+  animations: [routerTransition],
 
   styleUrls: ['./article.component.css']
 })
@@ -21,31 +22,31 @@ export class ArticleComponent implements OnInit {
   loading = false;
 
   constructor(private route: ActivatedRoute,
-     private _appComponent: AppComponent,
-     private titleService: Title,
-     private meta: Meta
-    ) { }
+    private _appComponent: AppComponent,
+    private titleService: Title,
+    private _location: Location,
+    private meta: Meta
+  ) { }
 
 
-htmlDecode(input)
-{
-  let doc = new DOMParser().parseFromString(input, "text/html");
-  return doc.documentElement.textContent;
-}
+  htmlDecode(input) {
+    let doc = new DOMParser().parseFromString(input, "text/html");
+    return doc.documentElement.textContent;
+  }
 
   ngOnInit() {
 
-    window.scrollTo(0,0);
+    window.scrollTo(0, 0);
 
     this.route.url.subscribe(segments => {
       let urlArray: string[] = [];
       segments.forEach(element => {
         urlArray.push(element.path);
       });
-      
+
 
       let slug: string = '/' + urlArray.join('/');
-      
+
 
       if (!this._appComponent.articles) {
         this.loading = true;
@@ -64,9 +65,12 @@ htmlDecode(input)
   }
 
   setArticle(articles, slug) {
-    let arts = articles.filter(function (article) {              
-      return article.slug === slug || decodeURI(article.slug) === slug;
+    let arts = articles.filter(function (article) {
+      return article.slug === slug || decodeURI(article.slug) === slug || article.slug === decodeURI(slug);
     });
+
+    this._location.replaceState(decodeURI(slug));
+
 
     if (arts && arts.length > 0) {
       this.article = arts[0];
@@ -76,8 +80,8 @@ htmlDecode(input)
       this.article.teaser = 'Ceci est une erreur quatre cent quatre.'
       this.article.body = 'Désolé, la page demandée n\'a pas été trouvée.<br><br>';
     }
-    
-    this.titleService.setTitle(this.htmlDecode(this.article.title) +" | Loutre.ch");
+
+    this.titleService.setTitle(this.htmlDecode(this.article.title) + " | Loutre.ch");
     this.meta.updateTag({ name: 'description', content: this.article.teaser });
   }
 
