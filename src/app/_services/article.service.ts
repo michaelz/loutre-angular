c1bd1b23a398bd92a4c4cc296a8a0b65662aff7a
Michael Zwyssig <mzwyssig@3ne.ch>
2018-06-26 14:23:30 +0200

    https url fix

--- src/app/_services/article.service.ts
+++ src/app/_services/article.service.ts
@@ -7,16 +7,10 @@ export class ArticleService {
     constructor(public http: Http) { }
 
     getData() {
-        return this.http.get("http://api.loutre.ch")
-            .map((res: Response) => { 
-                let result;
-                
-                result = res.json().map(article => {
+        return this.http.get("https://api.loutre.ch")
+            .map((res: Response) => res.json().map(article => {
                     article.slug =  decodeURI(article.slug)
                     return article;
-                })
-                return result;
-            
-            }); //records in this case
+                }));
     }
 }
